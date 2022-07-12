beb37cd86f17cd30e119ee9b915573e5a42d6928
redvoxdev <redvoxdev@gmail.com>
2022-07-13 00:34:38 +0600

    feat: add trash

---
+++ src/index.js
@@ -0,0 +1,11 @@
+module.exports = function reverse (n) {
+  let number = Math.abs(n);
+  let reversedNumber = 0; 
+
+  while (number > 0) {
+    reversedNumber = reversedNumber * 10 + number % 10;
+    number = Math.floor(number / 10);
+  }
+  
+  return reversedNumber;
+}
